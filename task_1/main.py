a3a67f0c55a2d3c94050d68ed007679c632123c8
rimmachepuxa <ch4epuxa@gmail.com>
2025-12-03 23:48:48 +0300

    Update main.py

--- task_1/main.py
+++ task_1/main.py
@@ -1,16 +1,16 @@
 # Чикида Римма
 str = input("Введите строку на русском: ")
-list_vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯ"
+list_vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯ" # Делаем список всех гласных 
 vowel_count = 0
-consonant_count = 0
+consonant_count = 0 # Счетчики гласных и согласных 
 for char in str:
-    if char.isalpha():  # Проверяем, что символ — буква
-        if char in list_vowels:
-            vowel_count += 1
-        else:
-            consonant_count += 1
+    if char.isalpha():  # Проходим по каждому символу 
+        if char in list_vowels: # Проверяем гласные 
+            vowel_count += 1 # Добавляем +1 к гласным
+        else: # Если не гласный символ
+            consonant_count += 1 # +1 к согласным 
 
-# Вывод результатов
+# Вывод данных 
 print("Длина строки:", len(str))
-print("Количество гласных букв:", vowel_count)
+print("Количество гласных букв:", vowel_count) 
 print("Количество согласных букв:", consonant_count)
